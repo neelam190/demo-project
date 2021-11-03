4dd0ff54bfe92a34b11bd500e93a5ec43e362284
neelam190 <93641520+neelam190@users.noreply.github.com>
2021-11-03 14:11:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { isLoggedIn } = require('./middleware');
 const http = require('http');
 const server = http.createServer(app);
 
-// 1. mongoose connection
+// mongoose connection
 mongoose.connect('mongodb://localhost:27017/demo-project',
     {
         useNewUrlParser: true,
@@ -31,32 +31,32 @@ app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, '/public')));
 // middleware for passing form data in login/signup
 app.use(express.urlencoded({ extended: true }));
-// 7. data coming from axios - frontend
+// data coming from axios - frontend
 app.use(express.json());
 
-// 5. import routes
+// import routes
 const authRoutes = require('./routes/auth');
 
-// 4. set sessions 
+// set sessions 
 app.use(session({
     secret: 'weneedasomebettersecret',
     resave: false,
     saveUninitialized: true,
 }));
 
-// 11. import connect-flash
+// import connect-flash
 app.use(flash());
 
-// 8. initialize passport - this will run for each incoming request
+// initialize passport - this will run for each incoming request
 app.use(passport.initialize());
 app.use(passport.session());
 
-// 9. strategy we'll use; serialize/deserialize
+// strategy we'll use; serialize/deserialize
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// 14. access flash messages
+// access flash messages
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
     res.render('layouts/main-layout');
 });
 
-// 6. using routes
+// using routes
 app.use(authRoutes);
 
 server.listen(process.env.PORT||3000, () => {
